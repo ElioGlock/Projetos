2002729e9a076aaa171bba6baf49b9b8d079f0b7
ElioGlock <eliosilva12356@gmail.com>
2025-07-09 10:10:42 -0300

    Update

--- Cifra_de_cesar.py
+++ Cifra_de_cesar.py
@@ -1,21 +1,59 @@
+import string
+import os
 
-maiusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-minusculas = "abcdefghijklmnopqrstuvwxyz"
+maiusculas = string.ascii_uppercase
+minusculas = string.ascii_lowercase
 
-texto = input("Digite o texto: ")
-chave = int(input("Digite a chave: "))
-cifrado = ""
+def limpar():
+    os.system("cls")
+    
+def codificar(cifrado):
+    for letra in texto:
+        if letra in maiusculas:
+            posicao = maiusculas.find(letra)
+            nova_posicao = (posicao + chave) % len(maiusculas)#O % faz com que volte para a identação da listam,usando com exemplo caso o valor 
+            cifrado += maiusculas[nova_posicao]               #seja 56 e passe da lista(26), essa função faz retornar.
+        elif letra in minusculas:
+            posicao = minusculas.find(letra)
+            nova_posicao = (posicao + chave) % len(minusculas)
+            cifrado += minusculas[nova_posicao]
+        else:
+            cifrado += letra
+    print(cifrado)
+    
+def descifrar(descifrar):
+    for letra in texto:
+        if letra in maiusculas:
+            posicao = maiusculas.find(letra)
+            nova_posicao = (posicao - chave) % len(maiusculas)#O % faz com que volte para a identação da listam,usando com exemplo caso o valor 
+            descifrar += maiusculas[nova_posicao]               #seja 56 e passe da lista(26), essa função faz retornar.
+        elif letra in minusculas:
+            posicao = minusculas.find(letra)
+            nova_posicao = (posicao - chave) % len(minusculas)
+            descifrar += minusculas[nova_posicao]
+        else:
+            descifrar += letra
+    print(descifrar)
 
-for letra in texto:
-    if letra in maiusculas:
-        posicao = maiusculas.find(letra)
-        nova_posicao = (posicao + chave) % len(maiusculas)#O % faz com que volte para a identação da listam,usando com exemplo caso o valor 
-        cifrado += maiusculas[nova_posicao]               #seja 56 e passe da lista(26), essa função faz retornar.
-    elif letra in minusculas:
-        posicao = minusculas.find(letra)
-        nova_posicao = (posicao + chave) % len(minusculas)
-        cifrado += minusculas[nova_posicao]
+    
+    
+opcoes = [0,1,""]
+
+while True:
+    inicio = int(input("Oque deseja fazer hoje?\n0 - codificar 1 - decifrar\n"))
+    limpar()
+    if inicio in opcoes:
+        if inicio == 0:
+            texto = input("Digite o texto: ")
+            chave = int(input("Digite a chave: "))
+            cifrado = ""
+            codificar(cifrado)
+            
+        elif inicio == 1:
+            texto = input("Digite o texto: ")
+            chave = int(input("Digite a chave: "))
+            descifrado = ""
+            descifrar(descifrado)
+        break
     else:
-        cifrado += letra
-
-print(cifrado)
+        continue
